f5e3c848035e17487acaa7f9e1a3c6bab0dbb031
gema_handika <gemahandika88@gmail.com>
2025-07-15 17:16:28 +0700

    halaman printer

--- app/controller/Printer.php
+++ app/controller/Printer.php
@@ -104,9 +104,6 @@ class Printer extends Controller
     }
     public function edit()
     {
-        ini_set('display_errors', 1);
-        ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $data = [
